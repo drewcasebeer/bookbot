6a1c951a3970d263c819f010e5c726bee9137912
Drew Casebeer <andrew.casebeer@gmail.com>
2025-12-18 18:57:20 -0600

    update filepath from sys commands

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_character_counts, get_sorted_dictionary
 
 
@@ -11,7 +12,11 @@ def get_book_text(filepath: str):
 
 
 def main():
-    file_contents = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        raise Exception("Usage: python3 main.py <path_to_book>")
+
+    filepath = sys.argv[1]
+    file_contents = get_book_text(filepath)
 
     num_words = get_num_words(file_contents)
     print(f"Found {num_words} total words")
